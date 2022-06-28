0d6360f5cb444c3d29e9df4bd8e74d0133894952
ariola <ariola.markou@gmail.com>
2022-06-28 11:30:46 +0200

    Added footer-GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App{" "}
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a
+          href="https://glittery-boba-1babca.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ariola Markou
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/ariolam/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
