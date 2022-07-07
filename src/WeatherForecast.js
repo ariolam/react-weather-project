733364054035eb5847642d788f5dec9769d25f80
ariola <ariola.markou@gmail.com>
2022-07-07 13:13:07 +0300

    Added the forecast API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,13 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+  let latitude = props.coordinates.lat;
+  let longtitude = props.coordinates.lon;
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=81816ad955060b90cb48be234d7ed923&units=metric`;
+  axios.get(url).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          <div className="Forecast-day">Mon</div>
+          <div className="Forecast-day mt-4">Mon</div>
           <WeatherIcon code="01d" size={36} />
           <div className="Forecast-temperature">
             <span className="Forecast-temperature-max"> 20°</span>
