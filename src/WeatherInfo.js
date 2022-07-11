eff70035022caa2f41cf2b5383569b310a55c09d
ariola <ariola.markou@gmail.com>
2022-07-11 17:25:08 +0300

    Last styling touches

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,19 +2,22 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemp from "./WeatherTemp";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
+      <ul className="description">
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{props.data.description}</li>
+        <li className="text-capitalize text-primary">
+          {props.data.description}
+        </li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
+        <div className="col-6 d-inline">
           <WeatherIcon code={props.data.icon} size={52} />
           <WeatherTemp celsius={props.data.temperature} />
         </div>
