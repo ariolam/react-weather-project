18b1dc90779d4db51c39b83616980e02a7cf54ec
ariola <ariola.markou@gmail.com>
2022-07-11 13:20:28 +0300

    Changed the API Key

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -37,9 +37,8 @@ export default function WeatherForecast(props) {
   } else {
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
-    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=81816ad955060b90cb48be234d7ed923&units=metric`;
-
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=6ce35ed588ab9855033b23ac0dbfd433&units=metric`;
+    console.log("url", url);
     axios.get(url).then(handleResponse);
-    return null;
   }
 }
